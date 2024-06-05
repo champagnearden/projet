2093af45da36c791e024afc823af3fbc1ff8516f
Arthur <arthur.bocquart@gmail.com>
2024-06-05 17:58:21 +0200

    precisebecause employes clients' return is void

--- routes/employe/employe.mjs
+++ routes/employe/employe.mjs
@@ -116,7 +116,13 @@ router.route('/:id').get(async (req, res, next) => {
                 surname: 1,
                 email: 1,
                 role: 1,
-                clients: 1
+                clients: {
+                    surname: 1,
+                    name: 1,
+                    email: 1,
+                    comptes: 1,
+                    cartes: 1
+                }
             }
         }
     ];
